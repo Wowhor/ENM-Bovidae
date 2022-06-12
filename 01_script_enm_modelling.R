e048a95f871a31417905747c0665949a58d68b3f
Wantidah <62598473+Wantidah@users.noreply.github.com>
2022-06-12 14:04:53 +1200

    Update and rename Script_enm_modelling.R to 01_script_enm_modelling.R

--- 01_script_enm_modelling.R
+++ 01_script_enm_modelling.R
@@ -1,4 +1,5 @@
 # ecological model for five Bovidae species in Thailand.
+# 
 # This can be applied to other occurrence dataset 
 # This code perform in R version 4.0.1 
 # Using ENMTML package for modelling [https://github.com/andrefaa/ENMTML] 
@@ -17,61 +18,65 @@ library(raster)
 library(dplyr)
 library(openxlsx)
 
-# file that we need for modelling 
-# 3 components: 
-
-# 1) occurrence data (sample of dataset from gbif.org) file name:      
-#       - bg_test.txt = gaur,
-#       - bj_test.txt = banteng,               
-#       - ba_test.txt = wild water buffalo       
+# Download the sample dataset and files for modelling here:https://drive.google.com/drive/folders/1tKMkeltE1eSnrmTh4obpIPjYEJaJTChX?usp=sharing 
+# 
+# Files are in the folder "data_preparation"
+# We used 3 components: Occurrence data (this code used bg_test.txt), environmental variables, accessible area (for restricting the species distribution)
+# 1) occurrence data (sample of dataset from gbif.org):      
+#       - bg_test.txt = Gaur,
+#       - bj_test.txt = Banteng,               
+#       - ba_test.txt = Wild water buffalo       
 #       - cs_test.txt = Mainland serow
 #       - ng_test.txt = Chinese goral
 # 2) 28 environmental variables: folder name 'env' -> inside contained the environmental variable for each species.
-#       - cropped environmental variables for five species based on accessible areas (the shapefile of accessible areas are in the shapefile folder)
+#       - These files were cropped based on accessible areas (the shapefile of accessible areas are in the data_preperation folder)
 #       - envgaur, envbanteng, envbuffalo, envserow, envgoral are the species-specific accessible area (SSA) which different for each species
-#       - env_la is a single large accessible area used for every species (LA)
+#       - env_la is a single large accessible area used in every species (LA)
 #       
 # 3) accessible areas shapefile (two types species-specific and large accessible areas)
-#       - folder 'shapefile' is contained SSA and LA shapefile for spatial restriction
-#       - accgaur, accbanteng, accbuffalo, accserow, accgoral are selected from ecoregions based on literature review and IUCN polygon
-#       - acc_la is a LA which covers all five species accessible areas
+#       - folder 'acc' is contained SSA and LA shapefile for spatial restriction
+#             - accgaur, accbanteng, accbuffalo, accserow, accgoral are SSA that selected from ecoregions based on literature review and IUCN polygon
+#             - acc_la is a LA which use for all five species 
+# create 4 models (SSA with spatially restriction, SSA without spatially restriction, LA with spatially restriction, and LA without spatially restriction)
 
-# 1) set up working directory -----------------
-# path <-  "..." this can be the folder path for the dataset.
+
+# 1) Start from set up working directory -----------------
 # path<-"/Users/whorpien/Library/CloudStorage/OneDrive-MasseyUniversity/R/1working/"
 
-path<-"./bovidae_enm/..path here.."
+path<-"./bovidae_enm/"
 
 setwd(path)
 
 d_ex <- file.path(getwd())
 d_ex
 
-# species specific-accessible area (SSA) 
-# use gaur as a sample dataset
-# species occurrence data (species,x,y)
-d_occ <- file.path(d_ex,"bg_test.txt")
+# 2) SSA Modelling ---------------------------
 
-occ<- read.table(file.path(d_ex, 'bg_test.txt'),header = T)
+# using gaur as a sample
+
+# species occurrence data contain three columns: species,x,y
+# occurrence data path
+d_occ <- file.path(d_ex,"data_preparation/bg_test.txt")
+
+occ<- read.table(file.path(d_ex, 'data_preparation/bg_test.txt'),header = T)
 
 str(occ)
 
-# 28 environmental variables 
-d_env <- file.path(d_ex, "envgaur")
+# 28 environmental variables path
+d_env <- file.path(d_ex, "data_preparation/env/envgaur")
 dir(d_env)
 
 # check accessible area shapefile:
-plot(shapefile(file.path(d_ex,"accgaur/gaur_acc_disv_2.shp")))
+plot(shapefile(file.path(d_ex,"data_preparation/acc/accgaur/gaur_acc_disv_2.shp")))
 
 # species specific accessible area shapefile path
-m_path <- file.path(d_ex,"accgaur/gaur_acc_disv_2.shp") 
+m_path <- file.path(d_ex,"data_preparation/acc/accgaur/gaur_acc_disv_2.shp") 
 
-# 2)  SSA Modelling ---------------------------
 
-# - Gaur SSA MSDM OBR  ----------------------
+# - Gaur SSA MSDM with spatially restriction (MSDM) OBR
 ENMTML(pred_dir = d_env, 
        proj_dir = NULL, 
-       result_dir = "ResultGaurTest_OBRmsdm", 
+       result_dir = "ResultGaurTest_SSA_OBR", 
        occ_file = d_occ,
        sp = 'species', 
        x = 'x', 
@@ -93,29 +98,31 @@ ENMTML(pred_dir = d_env,
        ensemble=c(method=c('W_MEAN'), metric='TSS'),
        extrapolation = FALSE, 
        cores = 2)
+       
 # clean memmory garbage
 gc()
 
-# Gaur SSA no MSDM  ----------------------
-path<-"./bovidae_enm/..path here.."
+# Next
+# Gaur SSA without MSDM  ----------------------
 
-setwd(path)
-
-d_ex <- file.path(getwd())
-d_ex
-
-# species specific-accessible area (SSA) 
-# use gaur as a sample dataset
-# species occurrence data (species,x,y)
+# occurrence data path
 d_occ <- file.path(d_ex,"bg_test.txt")
 
-# 28 environmental variables 
+occ<- read.table(file.path(d_ex, 'bg_test.txt'),header = T)
+
+str(occ)
+
+# 28 environmental variables path
 d_env <- file.path(d_ex, "envgaur")
 dir(d_env)
+
+# check accessible area shapefile:
+plot(shapefile(file.path(d_ex,"accgaur/gaur_acc_disv_2.shp")))
 
 # species specific accessible area shapefile path
 m_path <- file.path(d_ex,"accgaur/gaur_acc_disv_2.shp") 
 
+# Gaur SSA without MSDM OBR 
 ENMTML(pred_dir = d_env, 
        proj_dir = NULL, 
        result_dir = "ResultGaurTest_SSA_nomsdm", 
@@ -140,79 +147,33 @@ ENMTML(pred_dir = d_env,
        ensemble=c(method=c('W_MEAN'), metric='TSS'),
        extrapolation = FALSE, 
        cores = 2)
-
-# 3) LA modelling --------------------------------
-path<-"./bovidae_enm/..path here.."
-
-setwd(path)
-
-d_ex <- file.path(getwd())
-d_ex
-
-# species occurrence data (species,x,y)
-d_occ <- file.path(d_ex,"bg_test.txt")
-
-# 28 environmental variables 
-d_env <- file.path(d_ex, "env_la")
-dir(d_env)
-
-# check mask shapefile:
-m<-shapefile(file.path(d_ex,"acc_la/acc_ecoregion_msdm.shp"))
-plot(m)
-
-# large accessible area shapefile path
-m_path <- file.path(d_ex,"acc_la/acc_ecoregion_msdm.shp") 
-
-# - Gaur LA no MSDM------------
-ENMTML(pred_dir = d_env, 
-       proj_dir = NULL, 
-       result_dir = "ResultGaurTest_LA_OBR", 
-       occ_file = d_occ,
-       sp = 'species', 
-       x = 'x', 
-       y = 'y', 
-       min_occ = 10,
-       thin_occ=c(method='CELLSIZE'),
-       eval_occ = NULL, 
-       colin_var=c(method ='PCA'), 
-       imp_var = TRUE, 
-       sp_accessible_area = c(method= 'MASK', filepath= m_path ),
-       pseudoabs_method = c(method= 'RND'),    
-       pres_abs_ratio = 1, 
-       part = c(method='BOOT', replicates='10', proportion='0.75'),
-       save_part = FALSE, 
-       save_final = TRUE,
-       algorithm = c("BIO","GLM","GAM","SVM","RDF","MXD","MLK","GAU"), 
-       thr=c(type=c( 'MAX_TSS')),
-       msdm= NULL,
-       ensemble=c(method=c('W_MEAN'), metric='TSS'),
-       extrapolation = FALSE, 
-       cores = 2)
-
+       
+# clean memmory garbage
 gc()
 
-# - Gaur LA MSDM OBR------------
+# 3) LA modelling --------------------------------
 
-path<-"./bovidae_enm/..path here.."
+# Gaur LA with MSDM OBR ------------------
 
-setwd(path)
+# occurrence data path
+d_occ <- file.path(d_ex,"data_preparation/bg_test.txt")
 
-d_ex <- file.path(getwd())
-d_ex
+occ<- read.table(file.path(d_ex, 'bg_test.txt'),header = T)
 
-# species occurrence data (species,x,y)
-d_occ <- file.path(d_ex,"bg_test.txt")
+str(occ)
 
-# 28 environmental variables 
-d_env <- file.path(d_ex, "env_la")
+# 28 environmental variables path
+d_env <- file.path(d_ex, "data_preparation/env/env_la")
 dir(d_env)
 
 # check mask shapefile:
-m<-shapefile(file.path(d_ex,"acc_la/acc_ecoregion_msdm.shp"))
+m <-shapefile(file.path(d_ex,"data_preparation/acc/acc_la/acc_ecoregion_msdm.shp"))
 plot(m)
 
 # large accessible area shapefile path
-m_path <- file.path(d_ex,"acc_la/acc_ecoregion_msdm.shp") 
+m_path <- file.path(d_ex,"data_preparation/acc/acc_la/acc_ecoregion_msdm.shp") 
+
+# Gaur LA with MSDM OBR
 ENMTML(pred_dir = d_env, 
        proj_dir = NULL, 
        result_dir = "ResultGaurTest_LA_OBR", 
@@ -232,38 +193,35 @@ ENMTML(pred_dir = d_env,
        save_part = FALSE, 
        save_final = TRUE,
        algorithm = c("BIO","GLM","GAM","SVM","RDF","MXD","MLK","GAU"), 
-       thr=c(type=c('MAX_TSS',)),
+       thr=c(type=c('MAX_TSS')),
        msdm= c(method='OBR'),
        ensemble=c(method=c('W_MEAN'), metric='TSS'),
        extrapolation = FALSE, 
        cores = 2)
+       
+# clean memmory garbage       
 gc()
 
-# Gaur LA no MSDM
+# Gaur LA without MSDM OBR------------------
 
-path<-"./bovidae_enm/..path here.."
+# occurrence data path
+d_occ <- file.path(d_ex,"data_preparation/bg_test.txt")
 
-setwd(path)
-
-d_ex <- file.path(getwd())
-d_ex
-
-# species occurrence data (species,x,y)
-d_occ <- file.path(d_ex,"bg_test.txt")
-
-# 28 environmental variables 
-d_env <- file.path(d_ex, "env_la")
+# 28 environmental variables path
+d_env <- file.path(d_ex, "data_preparation/env/env_la")
 dir(d_env)
 
 # check mask shapefile:
-m<-shapefile(file.path(d_ex,"acc_la/acc_ecoregion_msdm.shp"))
+m <-shapefile(file.path(d_ex,"data_preparation/acc/acc_la/acc_ecoregion_msdm.shp"))
 plot(m)
 
 # large accessible area shapefile path
-m_path <- file.path(d_ex,"acc_la/acc_ecoregion_msdm.shp") 
+m_path <- file.path(d_ex,"data_preparation/acc/acc_la/acc_ecoregion_msdm.shp") 
+
+# Gaur LA without MSDM
 ENMTML(pred_dir = d_env, 
        proj_dir = NULL, 
-       result_dir = "ResultGaurTest", 
+       result_dir = "ResultGaurTest_SSA_nomsdm", 
        occ_file = d_occ,
        sp = 'species', 
        x = 'x', 
@@ -285,5 +243,6 @@ ENMTML(pred_dir = d_env,
        ensemble=c(method=c('W_MEAN'), metric='TSS'),
        extrapolation = FALSE, 
        cores = 2)
-
+       
+# clean memmory garbage
 gc()
